6a7117e5868ffad9bf32cf4f3d01c11f855a7c85
Sergio GF <serchgf@gmail.com>
2023-02-14 20:19:06 -0600

    add Ejercicio_03

---
+++ python_03.py
@@ -0,0 +1,13 @@
+print("hola")
+
+nombre = input("Dime tu nombre: ")
+edad = input("Dime tu edad: ")
+ciudad = input("Dime tu ciudad: ")
+edo_civil = input("Dime tu Estado Civil: ")
+num_1 = input("Dame un numero entre 1-100: ")
+num_2 = input("Dame un numero entre 100-200: ")
+division = int(num_1)/int(num_2)
+print(f"Mi nombre es {nombre}, mi edad es {edad}, soy de la ciudad {ciudad} y soy {edo_civil}"
+      f" y mi resultado de la division es: {division}")
+
+
