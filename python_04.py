6696d95e61cb3b5a8beca45d4027cf8c9308321d
Sergio GF <serchgf@gmail.com>
2023-02-15 19:37:09 -0600

    add Ejercicio_04

---
+++ python_04.py
@@ -0,0 +1,15 @@
+
+
+num_1 = int(input("Dame un numero entre 1-100: "))
+num_2 = int(input("Dame un numero entre 100-200: "))
+suma= num_1+num_2
+resta= num_1-num_2
+multiplicacion=num_1*num_2
+division_entera= int(num_2/num_1)
+resto_division=''
+
+print(f'la varuiable suma es: {suma}')
+print(f'la varuiable resta es: {resta}')
+print(f'la varuiable multipñicacion es: {multiplicacion}')
+print(f'la division entera es: {division_entera}')
+print(f'la variable resto division es: {resto_division}')
