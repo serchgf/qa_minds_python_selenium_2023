fdc7c47ecb68308dabdce191d4881c9ba241354a
Sergio GF <serchgf@gmail.com>
2023-02-16 19:43:01 -0600

    add Ejercicio_04

--- python_04.py
+++ python_04.py
@@ -1,15 +1,23 @@
 
 
-num_1 = int(input("Dame un numero entre 1-100: "))
-num_2 = int(input("Dame un numero entre 100-200: "))
-suma= num_1+num_2
-resta= num_1-num_2
-multiplicacion=num_1*num_2
-division_entera= int(num_2/num_1)
-resto_division=''
+num_1 = input("Dame un numero entero: ")
+num_2 = input("Dame un numero entero: ")
+num_1 = int(num_1)
+num_2 = int(num_2)
+suma = num_1+num_2
+resta = num_1-num_2
+multiplicacion = num_1 * num_2
+division_entera= num_1 // num_2
+resto_division = num_1 % num_2
 
-print(f'la varuiable suma es: {suma}')
-print(f'la varuiable resta es: {resta}')
-print(f'la varuiable multipñicacion es: {multiplicacion}')
+print(f'la variable suma es: {suma}')
+print(f'la variable resta es: {resta}')
+print(f'la variable multiplicacion es: {multiplicacion}')
 print(f'la division entera es: {division_entera}')
 print(f'la variable resto division es: {resto_division}')
+
+print(f'El primer numero es mayor que el segundo: {num_1>num_2}')
+print(f'El primer numero es menor que el segundo: {num_1<num_2}')
+print(f'El primer numero es igual que el segundo: {num_1==num_2}')
+print(f'El primer numero es diferente que el segundo: {num_1!=num_2}')
+
