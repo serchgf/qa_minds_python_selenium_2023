93871986567d072744b87c2dbc9d1252055e7364
Sergio GF <serchgf@gmail.com>
2023-02-21 19:53:23 -0600

    add Ejercicio_06_01

--- python_06_01.py
+++ python_06_01.py
@@ -1,6 +1,7 @@
 """
 Escribe un script que dado la edad de una persona y su altura pueda determinar si la misma puede o no subirse en la montaña rusa “Push-Pull”, dado que se debe ser mayor a 14 años y tener
 una altura no menor de 1,62. El script debe ser capaz de informar si puede o no subirse y en el caso de que no, porque razon (Si por edad, por tamaño u ambas) (edited)
+
 """
 
 
