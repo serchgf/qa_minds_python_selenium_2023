78b981d7b7841629c01896972ab054aa638c6316
Sergio GF <serchgf@gmail.com>
2023-03-09 18:58:11 -0600

    add completo opciones extras de update falta delete

--- actividad_intermedia.py
+++ actividad_intermedia.py
@@ -7,30 +7,48 @@ El sistema debe permitir mostrar TODOS los Cursos existentes, como también la p
 """
 
 
-def add_course(SYSTEM_COURSES: dict, courses_list: list):
-    print("Dar de alta un Curso:")
-
-    course_name = input("Ingresa nombre del curso: ")
-    num_of_students = input("Ingresa numero de estudiantes: ")
-    status = input("Cual es el estado?\nACTIVO\nNO INICIADO\nTu respuesta: ")
+def add_course():
+    print("\nIngresa un nuevo curso: ")
+    course_name = input("Ingresa el nombre del curso: ")
+    num_students = input("Ingresa numero de alumnos: ")
+    status = input("Ingresa el STATUS del curso\nINICIADO\nNO INICIADO\nTu respuesta:  ")
 
     nueva_entrada = {
-        "course_name": course_name,
-        "num_of_students": num_of_students,
-        "status": status
+        "COURSE_NAME": course_name,
+        "NUM_STUDENTS": num_students,
+        "STATUS": status,
     }
-    courses_list.append(nueva_entrada)
+    COURSES_LIST.append(nueva_entrada)
 
 
 COURSES_LIST = []
-def show_courses(SYSTEM_COURSES: dict, COURSES_LIST: list):
-    print(f"Los cursos actuales son: \n{COURSES_LIST}")
+def show_courses():
+    n_courses = str(len(COURSES_LIST))
+    print(f"\nLos cursos actuales en el sistema son {n_courses}: \n{COURSES_LIST}")
 
 
-def search_course(COURSES_LIST: list, course_name: str):
+def search_course():
+    course_name = input("Ingresa nombre de curso: ")
+    cont = 0
+    existe = False
+    # comprobar si el curso existe en la lista de diccionariosADD
+
     for course in COURSES_LIST:
-        if course['course_name'] == course_name:
-            print(course)
+        if course['COURSE_NAME'] != course_name:
+            cont += 1
+    if cont == len(COURSES_LIST):
+        print(f"El curso {course_name} no esta en la lista")
+        return False
+    else:
+        existe = True
+    # buscar el nombre del curso e imprimir la entrada
+    if existe:
+        for course in COURSES_LIST:
+            if course['COURSE_NAME'] == course_name:
+                print(course)
+                return course
+                # print(course)
+
 
 def delete_course(COURSES_LIST: list):
     ACTIONS = ("YES", "NO ")
@@ -39,39 +57,38 @@ def delete_course(COURSES_LIST: list):
         course_name = input("Ingresa el nombre del curso a eliminar:  ")
         for course in COURSES_LIST:
             if course['course_name'] == course_name:
-                course['course_name'].pop()
+                pop(course['course_name'])
                 print(f"se elimino: {course_name}")
             else:
                 print("Curso no existe, no hubo modificaciones")
 
-def update_status(COURSES_LIST: list):
-    ACTIONS = ("YES", "NO ")
-    respuesta = input("Deseas actualizar el estatus?:\nYES\nNO\nTu respuesta:  ")
-    if respuesta == ACTIONS[0]:
-        course_name = input("Ingresa el nombre del curso a actualizar status:  ")
-        new_status = input("Ingresa el nuevo Status:  ")
-        for course in COURSES_LIST:
-            if course['course_name'] == course_name:
-                course['status'] = new_status
-                print(course['status'])
-            else:
-                print("Curso no existe, no hubo modificaciones")
+def update_course():
+    print("\nQuieres quieres actualizar una entrada?\n")
+    ACTIONS = ["YES", "NO"]
+    action = input('|'.join(ACTIONS) + '|: ')
+    if action == ACTIONS[0]:
+        print("\nIngresa nombre de curso que quieres actualizar: ")
+        course = SEARCH_COURSE()
+        if course is not False:
+            print("\nQue dato quieres actualizar?\n")
+            DATA = ["COURSE_NAME", "NUM_STUDENTS", "STATUS"]
+            respuesta = input('|'.join(DATA) + '|: ')
+            if respuesta == DATA[0]:
+                course_name = input("Ingresa el nuevo nombre del curso: ")
+                course['COURSE_NAME'] = course_name
 
-def update_varios(COURSES_LIST: list):
-    ACTIONS = ("YES", "NO ")
-    respuesta = input("Deseas actualizar el estatus?:\nYES\nNO\nTu respuesta:  ")
-    if respuesta == ACTIONS[0]:
-        course_name = input("Ingresa el nombre del curso a actualizar status:  ")
-        new_status = input("Ingresa el nuevo Status:  ")
-        for course in COURSES_LIST:
-            if course['course_name'] == course_name:
-                course['status'] = new_status
-                print(course['status'])
-            else:
-                print("Curso no existe, no hubo modificaciones")
+            elif respuesta == DATA[1]:
+                num_students = input("Ingresa nuevo numero de alumnos: ")
+                course['NUM_STUDENTS'] = num_students
 
+            elif respuesta == DATA[2]:
+                status = input("Ingresa el nuevo STATUS del curso\nINICIADO\nNO INICIADO\nTu respuesta:  ")
+                course['STATUS'] = status
 
-SYSTEM_COURSES = {}
+            print(f"Tu entrada actualizada es la siguiente:\n{course}")
+        else:
+            pass
+
 
 ACTIONS = ("SEARCH", "ADD", "UPDATE", "PRINT", "DELETE", "EXIT ")
 
@@ -79,17 +96,17 @@ while True:
     actions_list = " | ".join(ACTIONS)
     action = input(f"Seleccione una accion: {actions_list}")
     if action == ACTIONS[0]:
-        course_to_search = input("Ingresa nombre del curso a buscar: ")
-        search_course(COURSES_LIST, course_to_search)
+        search_course()
     elif action == ACTIONS[1]:
-        add_course(SYSTEM_COURSES, COURSES_LIST)
+        add_course()
     elif action == ACTIONS[2]:
-        update_status(COURSES_LIST,)
+        update_course()
     elif action == ACTIONS[3]:
-        show_courses(SYSTEM_COURSES, COURSES_LIST)
+        show_courses()
     elif action == ACTIONS[4]:
-        delete_course(COURSES_LIST)
+        pass
     elif action == ACTIONS[5]:
+        print("\nCerrando Sistema")
         break
     else:
         print(f"Operacion no soportada: {action}")
